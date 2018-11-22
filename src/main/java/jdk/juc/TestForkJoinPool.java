92316fc2cba9cd5415863731ae2c0cee905c4685
dongfang.ding <1041765757@qq.com>
2018-11-22 17:51:29 +0800

    增加Fork/Join框架对无返回值的并行处理示例

--- src/main/java/jdk/juc/TestForkJoinPool.java
+++ src/main/java/jdk/juc/TestForkJoinPool.java
@@ -1,9 +1,11 @@
 package jdk.juc;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.RecursiveTask;
 import java.util.stream.LongStream;
 
@@ -11,7 +13,8 @@ import org.junit.jupiter.api.Test;;
 
 public class TestForkJoinPool {
 
-    public static void main(String[] args) {
+    @Test
+    public void test() {
         Instant start = Instant.now();
 
         ForkJoinPool pool = new ForkJoinPool();
@@ -60,6 +63,26 @@ public class TestForkJoinPool {
         System.out.println("耗费时间为：" + Duration.between(start, end).toMillis());//1536-8118
     }
 
+
+    /**
+     * 无返回值的Fork/Join示例
+     */
+    @Test
+    public void test3() {
+        ForkJoinPool forkJoinPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors() / 2);
+        Object object = new Object();
+        ReadExcelAction readExcelAction = new ReadExcelAction(object, 0L, 140000L);
+        forkJoinPool.invoke(readExcelAction);
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
 class ForkJoinSumCalculate extends RecursiveTask<Long> {
@@ -72,7 +95,7 @@ class ForkJoinSumCalculate extends RecursiveTask<Long> {
     private long start;
     private long end;
 
-    private static final long THURSHOLD = 10000L;  //临界值
+    private static final long THURSHOLD = 1000L;  //临界值
 
     public ForkJoinSumCalculate(long start, long end) {
         this.start = start;
@@ -105,3 +128,44 @@ class ForkJoinSumCalculate extends RecursiveTask<Long> {
     }
 
 }
+
+
+/**
+ * 无返回值的Fork/Join示例
+ * 如读取Excel，每行之间互不影响，则可以使用让线程同时读不同区间的行号
+ */
+class ReadExcelAction extends RecursiveAction {
+
+    private long start;
+    private long end;
+    // 具体要做的任务类
+    private Object object;
+
+    private static final long THURSHOLD = 10;
+
+    public ReadExcelAction(Object object, long start, long end) {
+        this.object = object;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected void compute() {
+        if ((end - start) < THURSHOLD) {
+            for (long i = start; i <= end; i++) {
+                System.out.println(Thread.currentThread().getName() + "读取行号： " + i);
+                object.hashCode();
+            }
+        } else {
+            long middle = (end + start) / 2;
+
+            ReadExcelAction left = new ReadExcelAction(object, start, middle);
+            left.fork();
+
+            ReadExcelAction right = new ReadExcelAction(object, middle + 1, end);
+            right.fork();
+        }
+    }
+}
+
+
